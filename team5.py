74bcbefde85a84d044fb0ac1c5723a1f16bccfd0
ryan21113 <44213684+ryan21113@users.noreply.github.com>
2018-12-01 18:33:26 -0800

    Add files via upload

--- team5.py
+++ team5.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'G-5' # Only 10 chars displayed.
+strategy_name = 'Betray, then betray or collude'
+strategy_description = 'Betray first round. If all scores combined is higher than zero betray, if lowwer collude.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,8 +26,13 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
+    all_score = my_score + their_score
+    if my_score == 0:
+        return 'b'
+    if all_score > 0:
+        return 'b'
+    else:
+        return 'c'
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
